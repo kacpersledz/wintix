13289e53a9918977d6e15dbe84eee2ae096219ad
kacpersledz <casper.sledx@gmail.com>
2026-08-31 23:20:24 +0200

    Clean up NixOS configuration

--- modules/base.nix
+++ modules/base.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   nix.settings.experimental-features = [
@@ -6,13 +6,10 @@
     "flakes"
   ];
 
-  # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "Europe/Warsaw";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
@@ -27,16 +24,12 @@
     LC_TIME = "pl_PL.UTF-8";
   };
 
-  # Configure console keymap
   console.keyMap = "pl2";
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    vim
     wget
     fastfetch
   ];
