2f88cc49acbb9950aab3622db663320ee72bb892
kacpersledz <casper.sledx@gmail.com>
2026-08-31 23:09:38 +0200

    Make Wintix commands location-independent (#6)

--- home/january/default.nix
+++ home/january/default.nix
@@ -10,7 +10,7 @@
   home.stateVersion = "26.05";
 
   programs.zsh.shellAliases = {
-    rebuild = "sudo nixos-rebuild switch --flake .#desktop";
-    update = "nix flake update && sudo nixos-rebuild switch --flake .#desktop";
+    rebuild = "sudo nixos-rebuild switch --flake ~/.wintix#desktop";
+    update = "nix flake update --flake ~/.wintix && sudo nixos-rebuild switch --flake ~/.wintix#desktop";
   };
 }
