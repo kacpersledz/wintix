ade6f5bb5da7a416f86de534a4a256f6c786069b
kacpersledz <casper.sledx@gmail.com>
2026-08-31 22:56:18 +0200

    Migrate Zsh configuration to Home Manager

--- home/january/default.nix
+++ home/january/default.nix
@@ -1,7 +1,16 @@
 { ... }:
 
 {
+  imports = [
+    ../shared/zsh.nix
+  ];
+
   home.username = "january";
   home.homeDirectory = "/home/january";
   home.stateVersion = "26.05";
+
+  programs.zsh.shellAliases = {
+    rebuild = "sudo nixos-rebuild switch --flake .#desktop";
+    update = "nix flake update && sudo nixos-rebuild switch --flake .#desktop";
+  };
 }
