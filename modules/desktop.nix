d4ab1a41f866199c0fc452d4ffc624512d27c1c8
kacpersledz <casper.sledx@gmail.com>
2026-09-07 12:48:15 +0200

    fix: match Plasma login background to desktop

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -1,9 +1,24 @@
 { pkgs, ... }:
 
+let
+  pathWallpaper = "${pkgs.kdePackages.plasma-workspace-wallpapers}/share/wallpapers/Path/contents/images/2560x1600.jpg";
+  sddmTheme = pkgs.runCommand "wintix-sddm-theme" { } ''
+    mkdir -p "$out/share/sddm/themes"
+    cp -r ${pkgs.kdePackages.plasma-desktop}/share/sddm/themes/breeze "$out/share/sddm/themes/wintix"
+    chmod -R u+w "$out/share/sddm/themes/wintix"
+    cat > "$out/share/sddm/themes/wintix/theme.conf.user" <<EOF
+    [General]
+    background=${pathWallpaper}
+    EOF
+  '';
+in
 {
   services.xserver.enable = true;
 
-  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm = {
+    enable = true;
+    theme = "${sddmTheme}/share/sddm/themes/wintix";
+  };
   services.desktopManager.plasma6.enable = true;
 
   services.xserver.xkb = {
